06e43c5cca2a989fe395a95321cdd1c1631290f8
thiagoc789 <70195218+thiagoc789@users.noreply.github.com>
2021-12-15 01:37:25 -0500

    PROYEC 12-13 Terminadas
    Se pueden editar y eliminar contratos temporales

--- src/routes/ejecucion.js
+++ src/routes/ejecucion.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const Proyecto = require('../models/project');
 const Contrato = require('../models/contrato')
 
 // PARTE DE CONTRATOS
@@ -11,6 +10,62 @@ router.get('/ejecucion/contratost', async (req,res) =>{
 
 router.get('/ejecucion/crear-contratost', (req,res) =>{
     res.render('ejecucion/crear-contratos');
+});
+
+router.get('/ejecucion/modificar/:id', async (req, res) => {
+    const contratos = await Contrato.findByPk(req.params.id);
+    res.render('ejecucion/editar-contratos', { contratos });
+});
+
+router.delete('/ejecucion/eliminar/:id', async (req, res) => {
+    await Contrato.destroy({
+        where: {
+            id: req.params.id,
+        }
+    });
+    res.redirect('/ejecucion/contratost');
+})
+
+router.post('/ejecucion/editar-contratos/:id', async (req, res) => {
+    const { numero, nombre, cedula, fecha_inicio, fecha_fin, valor } = req.body;
+    const contratos = await Contrato.findByPk(req.params.id);
+    const errors = [];
+    if (!numero) {
+        errors.push({ text: 'Por favor inserte un numero de contrato' });
+    }
+    if (!nombre) {
+        errors.push({ text: 'Por favor inserte un nombre' });
+    }
+    if (!cedula) {
+        errors.push({ text: 'Por favor inserte una cedula' });
+    }
+    if (!fecha_inicio) {
+        errors.push({ text: 'Por favor inserte una fecha de inicio' });
+    }
+    if (!fecha_fin) {
+        errors.push({ text: 'Por favor inserte una fecha de fin' });
+    }
+    if (!valor) {
+        errors.push({ text: 'Por favor inserte un valor' });
+    }
+    if (errors.length > 0) {
+        res.render('ejecucion/editar-contratos', {
+            contratos,
+            numero,
+            nombre,
+            cedula,
+            fecha_inicio,
+            fecha_fin,
+            valor
+        });
+    } else {
+        await Contrato.update({ numero: numero, nombre: nombre, cedula: cedula, fecha_inicio: fecha_inicio, fecha_fin: fecha_fin, valor: valor  }, {
+            where: {
+                id: req.params.id,
+            }
+        });
+        res.redirect('/ejecucion/contratost');
+    }
 });
 
 router.post('/ejecucion/nuevo-contratost', async (req, res) => {
