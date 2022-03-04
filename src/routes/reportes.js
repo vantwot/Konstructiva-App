9b1de0e1bd28690f425866f8be99e9e8c60c8f69
Dalex11 <54454160+Dalex11@users.noreply.github.com>
2022-03-04 18:19:18 -0500

    Creación Reportes Gráficos

---
+++ src/routes/reportes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const Proyecto = require('../models/project');
+const Anticipo = require('../models/anticipos');
+const Cliente = require('../models/cliente');
+const Requerimiento = require('../models/requerimiento')
+const Contrato = require('../models/contrato')
+
+router.get('/reportes/info-proyectos', async(req, res) => {
+    const proyectos = await Proyecto.findAll({ order: [ [ 'date', 'DESC' ] ], include: [ Cliente, Contrato ]});
+    res.render("reportes/reportesProyecto", {proyectos});
+});
+
+router.get('/reportes/info-contratost', async (req,res) =>{
+    const contratos = await Contrato.findAll();
+    res.render("reportes/reporteContratosT.hbs", { contratos });
+});
+
+router.get('/reportes/info-grafica1', async (req,res) =>{
+    const proyectos = await Proyecto.findAll({ order: [ [ 'date', 'DESC' ] ], include: [ Cliente, Contrato ]});
+    res.render("reportes/reporteGrafico1.hbs", { proyectos });
+});
+module.exports = router;
